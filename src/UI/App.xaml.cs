f2fcdfcc4a0654c107a5ef8fcf692d6334cbbdcf
yegor <astery227@gmail.com>
2024-03-18 21:01:54 +0300

    initial commit

---
+++ src/UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using PackageDelivery.Delivery;
+
+namespace PackageDelivery
+{
+    public partial class App
+    {
+        public App()
+        {
+            DBHelper.Init(@"Server=localhost;Database=PackageDelivery;User Id=packageDelivery;Password=pd;");
+        }
+    }
+}
