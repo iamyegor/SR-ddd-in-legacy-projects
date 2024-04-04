945336f42f328f343edaca1e1415069d0e069826
yegor <astery227@gmail.com>
2024-04-04 19:24:22 +0300

    Implement BubbleOutboxDeliverySynchronizer.Sync()

---
+++ ACL/Synchronizers/Delivery/BubbleOutboxDeliverySynchronizer.cs
@@ -0,0 +1,34 @@
+using ACL.Synchronizers.Delivery.Models;
+using ACL.Synchronizers.Delivery.Repositories;
+
+namespace ACL.Synchronizers.Delivery;
+
+public class BubbleOutboxDeliverySynchronizer
+{
+    private readonly BubbleOutboxRepository _bubbleOutboxRepository;
+    private readonly LegacyDeliveryRepository _legacyDeliveryRepository;
+
+    public BubbleOutboxDeliverySynchronizer(
+        BubbleOutboxRepository bubbleOutboxRepository,
+        LegacyDeliveryRepository legacyDeliveryRepository
+    )
+    {
+        _bubbleOutboxRepository = bubbleOutboxRepository;
+        _legacyDeliveryRepository = legacyDeliveryRepository;
+    }
+
+    public void Sync()
+    {
+        (List<int>? ids, List<DeliveryInLegacy>? legacyDeliveries) =
+            _bubbleOutboxRepository.Get<DeliveryInLegacy>("Delivery");
+
+        if (ids.Count == 0)
+        {
+            return;
+        }
+
+        _legacyDeliveryRepository.Save(legacyDeliveries);
+
+        _bubbleOutboxRepository.Remove(ids);
+    }
+}
