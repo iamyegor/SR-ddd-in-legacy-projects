df3e7ec56afcd069d85918d0e05afe78871a9b25
yegor <astery227@gmail.com>
2024-04-04 13:41:27 +0300

    Add outbox table to bubble and implement BubbleOutboxRepository.Save()

--- ACL/Synchronizers/Delivery/Repositories/BubbleOutboxRepository.cs
+++ ACL/Synchronizers/Delivery/Repositories/BubbleOutboxRepository.cs
@@ -1,17 +1,33 @@
 ﻿using ACL.Synchronizers.Delivery.Models;
+using Dapper;
+using Newtonsoft.Json;
 using Npgsql;
 
 namespace ACL.Synchronizers.Delivery.Repositories;
 
 public class BubbleOutboxRepository
 {
+    private readonly NpgsqlConnection _connection;
+    private readonly NpgsqlTransaction _transaction;
+
     public BubbleOutboxRepository(NpgsqlConnection connection, NpgsqlTransaction transaction)
     {
-        throw new NotImplementedException();
+        _connection = connection;
+        _transaction = transaction;
     }
 
-    public void Save(List<DeliveryInLegacy> mappedDeliveries, string delivery)
+    public void Save(List<DeliveryInLegacy> deliveriesToSave, string type)
     {
-        throw new NotImplementedException();
+        var deliveriesAsJson = deliveriesToSave.Select(delivery => new
+        {
+            Content = JsonConvert.SerializeObject(delivery)
+        });
+
+        string query =
+            @$"
+            insert into outbox (content, type)
+            values (@Content, '{type}')";
+
+        _connection.Execute(query, deliveriesAsJson, transaction: _transaction);
     }
 }
