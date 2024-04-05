8826036b5750e3d764c4b355a48d0d577ae8780d
yegor <astery227@gmail.com>
2024-04-05 11:49:01 +0300

    Implement GetAllThatNeedSync and SetSyncFlagFalseForQueriedProducts in LegacyProductRepository

---
+++ ACL/Synchronizers/Product/Repositories/LegacyProductRepository.cs
@@ -0,0 +1,45 @@
+﻿using ACL.Synchronizers.Product.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace ACL.Synchronizers.Product.Repositories;
+
+public class LegacyProductRepository
+{
+    private const string TempTable = "#products_to_sync";
+    private readonly SqlConnection _connection;
+    private readonly SqlTransaction _transaction;
+
+    public LegacyProductRepository(SqlConnection connection, SqlTransaction transaction)
+    {
+        _connection = connection;
+        _transaction = transaction;
+    }
+
+    public List<ProductInLegacy> GetAllThatNeedSync()
+    {
+        string query =
+            @$"select 
+                NMB_CM, NM_CLM, WT, WT_KG 
+            into {TempTable}
+            from PRD_TBL with (updlock)
+            where IsSyncNeeded = 1
+            
+            select *
+            from {TempTable}";
+
+        return _connection.Query<ProductInLegacy>(query, transaction: _transaction).ToList();
+    }
+
+    public void SetSyncFlagFalseForQueriedProducts()
+    {
+        string query =
+            @$"
+            update p
+            set p.IsSyncNeeded = 0
+            from PRD_TBL p
+            inner join {TempTable} t on t.NMB_CM = p.NMB_CM";
+
+        _connection.Execute(query, transaction: _transaction);
+    }
+}
