e049a05910f65e3cc01df885ecf649df874c6593
yegor <astery227@gmail.com>
2024-04-05 10:37:24 +0300

    Implement BubbleDeliveryRepository.GetAllThatNeedSync()

--- ACL/Synchronizers/Delivery/BubbleDeliveryRepository.cs
+++ ACL/Synchronizers/Delivery/BubbleDeliveryRepository.cs
@@ -1,18 +1,35 @@
 ﻿using ACL.Synchronizers.Delivery.Models;
+using Dapper;
 using Npgsql;
 
 namespace ACL.Synchronizers.Delivery;
 
 public class BubbleDeliveryRepository
 {
+    private const string TempTable = "deliveries_to_sync";
+    private readonly NpgsqlConnection _connection;
+    private readonly NpgsqlTransaction _transaction;
+
     public BubbleDeliveryRepository(NpgsqlConnection connection, NpgsqlTransaction transaction)
     {
-        throw new NotImplementedException();
+        _connection = connection;
+        _transaction = transaction;
     }
 
     public List<DeliveryInBubble> GetAllThatNeedSync()
     {
-        throw new NotImplementedException();
+        string query =
+            @$"
+            select id as Id, cost_estimate as CostEstimate
+            into temp {TempTable}
+            from deliveries
+            where is_sync_needed = true
+            for update;
+
+            select *
+            from {TempTable}";
+
+        return _connection.Query<DeliveryInBubble>(query, transaction: _transaction).ToList();
     }
 
     public void SetSyncFlagsFalseForQueried()
