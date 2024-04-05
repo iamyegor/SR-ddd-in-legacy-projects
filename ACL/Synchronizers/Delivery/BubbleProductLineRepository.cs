8b0d88dae06c44b5d0be4ced5a75771ddf043401
yegor <astery227@gmail.com>
2024-04-05 10:41:04 +0300

    Implement GetAllThatNeedSync() in BubbleProductLineRepository

--- ACL/Synchronizers/Delivery/BubbleProductLineRepository.cs
+++ ACL/Synchronizers/Delivery/BubbleProductLineRepository.cs
@@ -1,17 +1,37 @@
 ﻿using ACL.Synchronizers.Delivery.Models;
+using Dapper;
 using Npgsql;
 
 namespace ACL.Synchronizers.Delivery;
 
 public class BubbleProductLineRepository
 {
+    private const string TempTable = "product_lines_to_sync";
+    private readonly NpgsqlConnection _connection;
+    private readonly NpgsqlTransaction _transaction;
+
     public BubbleProductLineRepository(NpgsqlConnection connection, NpgsqlTransaction transaction)
     {
-        throw new NotImplementedException();
+        _connection = connection;
+        _transaction = transaction;
     }
 
     public List<ProductLineInBubble> GetAllThatNeedSync()
     {
-        throw new NotImplementedException();
+        string query =
+            @$"
+            select 
+                product_id as ProductId,
+                amount as Amount,
+                delivery_id as DeliveryId
+            into temp {TempTable}
+            from product_lines
+            where delivery_id in (select id from deliveries where is_sync_needed = true)
+            for update;
+
+            select *
+            from {TempTable}";
+
+        return _connection.Query<ProductLineInBubble>(query, transaction: _transaction).ToList();
     }
 }
