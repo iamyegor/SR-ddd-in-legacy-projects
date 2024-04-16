9c28227fc58978789df744d4876f8ccdfa2a5c2f
yegor <astery227@gmail.com>
2024-04-16 14:48:53 +0300

    A: Implement a high-level skeletion of GetUpdatedAndResetFlags in BubbleDeliveryRepository

--- ACL/Synchronizers/Delivery/Repositories/BubbleDeliveryRepository.cs
+++ ACL/Synchronizers/Delivery/Repositories/BubbleDeliveryRepository.cs
@@ -7,6 +7,43 @@ public class BubbleDeliveryRepository
 {
     public List<DeliveryInBubble> GetUpdatedAndResetFlags(NpgsqlTransaction transaction)
     {
+        (List<int> deliveryIds, List<int> productLineIds) = LockDeliveriesAndProductLines(
+            transaction
+        );
+
+        List<DeliveryInBubble> deliveries = GetDeliveriesForSync(deliveryIds, transaction);
+
+        DeleteSoftDeletedProductLines(productLineIds, transaction);
+        ResetSyncFlags(deliveryIds, transaction);
+
+        return deliveries;
+    }
+
+    private (List<int> deliveryIds, List<int> productLineIds) LockDeliveriesAndProductLines(
+        NpgsqlTransaction transaction
+    )
+    {
+        throw new NotImplementedException();
+    }
+
+    private List<DeliveryInBubble> GetDeliveriesForSync(
+        List<int> deliveryIds,
+        NpgsqlTransaction transaction
+    )
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DeleteSoftDeletedProductLines(
+        List<int> productLineIds,
+        NpgsqlTransaction transaction
+    )
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ResetSyncFlags(List<int> deliveryIds, NpgsqlTransaction transaction)
+    {
         throw new NotImplementedException();
     }
 }
